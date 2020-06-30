6eacdcd06a5e2e32607af0903ec8b8c191143a7f
Samir Ettali <ettali.samir@gmail.com>
2020-06-30 15:20:22 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# DumpCN
+
+A simple script that reads a list of domains from standard input, grabs the
+certificate and prints the CN. By default it runs 32 threads (goroutines
+actually).
+
+Running it against
+[opendns-top-domains.txt](https://raw.githubusercontent.com/opendns/public-domain-lists/master/opendns-top-domains.txt)
+(10000 domains) takes approximately 1.5 seconds:
+```
+$ time ./dumpcn -workers=100 < opendns-top-domains.txt > /dev/null
+./dumpcn -workers=100 < opendns-top-domains.txt > /dev/null  1,66s user 1,24s system 39% cpu 7,311 total
+```
+
+## Installation
+
+```
+$ go get -u github.com/samirettali/dumpcn
+```
+
+## Usage
+
+```
+$ cat domains.txt | ./dumpcn
+```
+
+Change number of threads:
+```
+$ cat domains.txt | ./dumpcn -workers=100
+```
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+)
+
+func worker(jobChan <-chan string, resChan chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	var transport = &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+
+	var client = &http.Client{
+		Timeout:   time.Second * 10,
+		Transport: transport,
+	}
+
+	for {
+		job, ok := <-jobChan
+		if !ok {
+			return
+		}
+
+		req, reqErr := http.NewRequest("GET", job, nil)
+		if reqErr != nil {
+			continue
+		}
+
+		resp, clientErr := client.Do(req)
+		if clientErr != nil {
+			continue
+		}
+
+		if resp.TLS != nil && len(resp.TLS.VerifiedChains) > 0 && len(resp.TLS.VerifiedChains[0]) > 0 {
+			resChan <- resp.TLS.VerifiedChains[0][0].Subject.CommonName
+		}
+	}
+
+}
+func main() {
+	workers := flag.Int("workers", 32, "numbers of workers")
+	flag.Parse()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	jobChan := make(chan string)
+	resChan := make(chan string)
+	done := make(chan struct{})
+
+	var wg sync.WaitGroup
+	wg.Add(*workers)
+
+	go func() {
+		wg.Wait()
+		close(done)
+	}()
+
+	for i := 0; i < *workers; i++ {
+		go worker(jobChan, resChan, &wg)
+	}
+
+	go func() {
+		for scanner.Scan() {
+			jobChan <- scanner.Text()
+		}
+		if err := scanner.Err(); err != nil {
+			log.Println(err)
+		}
+		close(jobChan)
+	}()
+
+	for {
+		select {
+		case <-done:
+			return
+		case res := <-resChan:
+			fmt.Println(res)
+		}
+	}
+}
